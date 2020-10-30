fc265e9c311b81838ead9efb776e8712b5db703d
Dan7n <62748054+Dan7n@users.noreply.github.com>
2020-10-31 00:44:42 +0100

    Delete button
    Finally got javascript to find my delete buttons.. also added today's date (goona style that section later) and fixed a problem with my ifs

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
 let today = new Date();
 let time = today.getHours();
+let date = today.getFullYear() + "-" + today.getMonth() + "-" + today.getDate();
+let datePTag = document.getElementById("todaysDate");
 let timeGreetingHeader = document.getElementById("greetingHeading"); //header tag in the greetings section
 let userTextSubmission = document.getElementById("addTodo");
 let inputForm = document.getElementById("formElement");
 let newToDo = userTextSubmission.value;
 let ulElement = document.getElementById("ulElement");
+let submitButon = document.getElementById("submitTodo");
+let deleteButton = document.getElementsByClassName("delete");
 
-if (time > 0 && time < 12) {
+// console.log(deleteButton);
+
+
+
+
+// ifs and fors 
+
+if (time >= 0 && time < 12) {
     timeGreetingHeader.innerHTML = "Good morning!";
 } else if (time >= 12 && time <= 17) {
     timeGreetingHeader.innerHTML = "Good afternoon!";
@@ -14,8 +25,19 @@ if (time > 0 && time < 12) {
     timeGreetingHeader.innerHTML = "Good evening!";
 }
 
+//loop to add event listener to all delete buttons
+for (let i = 0; i < deleteButton.length; i++) {
+    deleteButton[i].addEventListener("click", () => { console.log("this works") });
+};
 
-inputForm.addEventListener("submit", () => {
+//functions
+
+function printOutDate() {
+    datePTag.innerHTML = date;
+}
+printOutDate();
+
+function createNewTodo() {
     event.preventDefault();
     const newLiElement = document.createElement("li"); //create li
     newLiElement.classList.add("list-item");
@@ -35,6 +57,7 @@ inputForm.addEventListener("submit", () => {
     deleteIcon.innerHTML = '<i class="fas fa-trash"></i>';
     const btnElement = document.createElement("input");
     btnElement.type = "button";
+    btnElement.classList.add("btn");
     btnElement.setAttribute("name", "delete");
     deleteIcon.appendChild(btnElement);
     containerDiv.appendChild(deleteIcon);
@@ -43,9 +66,19 @@ inputForm.addEventListener("submit", () => {
     userTextSubmission.value = "";
 
     //testing 
-    console.log("This is working");
-});
+    // console.log("This is working");
 
+}
+
+// function deleteTodo() {
+//     const currentTodoList =
+// }
+
+
+//event listeners 
+
+inputForm.addEventListener("submit", () => { createNewTodo() });
+submitButon.addEventListener("click", () => { createNewTodo() });
 
 
 //textbox onfocus, remove placeholder and fram around
