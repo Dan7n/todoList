33734dc1cd5991acc7d0e5d92d3aa0477ec37760
Danny Isaac <62748054+Dan7n@users.noreply.github.com>
2020-11-17 13:04:19 +0100

    Object-oriented project

--- js/main.js
+++ js/main.js
@@ -6,29 +6,24 @@ const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Fri
 const whatDayIsIt = daysOfWeek[today.getDay()];
 const timeGreetingHeader = document.getElementById("greetingHeading"); //header tag in the greetings section
 const dayOfWeekContainer = document.getElementById("dayOfWeekContainer"); //div where the day of week will be printed
+let listOfToDos = []; //all todos will be added to this array
 const userTextSubmission = document.getElementById("addTodo");
 const inputForm = document.getElementById("formElement");
 const newToDo = userTextSubmission.value;
 const todoSection = document.querySelector("#todoSection");
 const ulElement = document.querySelector("#ulElement");
+const onloadText = document.getElementById("onloadText");
+let nomoreTasks = ""; //gonna be added later when there are no more tasks to show
 let liveLiList = ulElement.getElementsByTagName("li");
 let listOfLiElements = [].slice.call(liveLiList);
-const hardcodedLiElements = document.querySelectorAll(".list-item") //using querySelectorAll so that I can use a forEach loop with it
 const pTagInsideUl = ulElement.getElementsByTagName("p");
 const submitButon = document.getElementById("submitTodo");
 const deleteButton = document.getElementsByClassName("delete");
 const svgIcon = document.getElementById("svgIcon");
 const svgPath = document.getElementById("svgPath");
-let closestLiElement;
-const array = [].slice.call(deleteButton); //convery deleteButton to an array
-const checkBoxes = document.getElementsByClassName("checkbox");
-const checkBoxesArray = [].slice.call(checkBoxes); //checkboxes converted to an array
-const allTodoDivs = document.getElementsByClassName("todo-item");
 let draggedItem;
 
-
-// ifs and fors ------------------------------
-
+// print out greeting based on time of day
 if (time >= 0 && time < 12) {
     timeGreetingHeader.innerHTML = "Good morning!";
 } else if (time >= 12 && time <= 17) {
@@ -36,34 +31,6 @@ if (time >= 0 && time < 12) {
 } else if (time >= 17 && time <= 23) {
     timeGreetingHeader.innerHTML = "Good evening!";
 }
-
-//loop to add event listener to all delete buttons
-for (let i = 0; i < array.length; i++) {
-    array[i].addEventListener("click", () => {
-        closestLiElement = array[i].closest("li");
-        closestLiElement.firstElementChild.classList.add("deleted");
-        const closestContainer = array[i].closest(".todo-item").classList.add("deleted");
-        ulElement.addEventListener("animationend", () => {
-            closestLiElement.remove();
-        });
-    });
-};
-
-//mark created tasks as complete
-for (let i = 0; i < checkBoxesArray.length; i++) {
-    let closestPTag = checkBoxesArray[i].nextElementSibling;
-    checkBoxesArray[i].addEventListener("click", () => {
-        if (checkBoxesArray[i].checked === true) {
-            closestPTag.classList.add("completed");
-            closestPTag.nextElementSibling.classList.add("label-completed");
-
-        } else if (checkBoxesArray[i].checked === false) {
-            closestPTag.classList.remove("completed");
-            closestPTag.nextElementSibling.classList.remove("label-completed");
-
-        };
-    });
-};
 
 //functions to print out todays date
 function printOutDate() {
@@ -132,68 +99,124 @@ todoSection.addEventListener("dragover", (e) => {
 });
 
 
+//class to create new objects
+class TodoMaker {
+    constructor(inputedText) {
+        this.inputedText = inputedText;
+    }
+};
+
+//defining a few tasks, then adding them to an array of tasks
+const task_1 = new TodoMaker("Learn Javascript");
+const task_2 = new TodoMaker("Reherse for upcoming gig");
+const task_3 = new TodoMaker("Finish up homework");
+listOfToDos.push(task_1, task_2, task_3);
+loopAndShowOnScreen();
+
+
+
+//create new object, add it to the array
 function createNewTodo() {
-    event.preventDefault();
-    const newLiElement = document.createElement("li"); //create li
-    newLiElement.classList.add("list-item");
-    newLiElement.setAttribute("draggable", "true");
-    const spanTag = document.createElement("span");
-    spanTag.classList.add("fas");
-    spanTag.classList.add("fa-bars");
-    newLiElement.appendChild(spanTag);
-    const containerDiv = document.createElement("div");
-    containerDiv.classList.add("todo-item");
-    newLiElement.appendChild(containerDiv);
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.classList.add("checkbox");
-    containerDiv.appendChild(checkbox);
-    const pElement = document.createElement("p");
-    pElement.innerText = userTextSubmission.value;
-    containerDiv.appendChild(pElement);
-    const deleteIcon = document.createElement("label");
-    deleteIcon.setAttribute("for", "delete");
-    deleteIcon.classList.add("delete");
-    deleteIcon.innerHTML = '<i class="fas fa-trash"></i>';
-    array.push(deleteIcon);
-    const btnElement = document.createElement("input");
-    btnElement.type = "button";
-    btnElement.classList.add("btn");
-    btnElement.setAttribute("name", "delete");
-    deleteIcon.appendChild(btnElement);
-    containerDiv.appendChild(deleteIcon);
-    newLiElement.appendChild(containerDiv);
-    ulElement.appendChild(newLiElement);
-    userTextSubmission.value = "";
+    if (userTextSubmission.value === "") {
+        alert("Please write something")
+    } else {
+        const newTaskObject = new TodoMaker(userTextSubmission.value);
+        listOfToDos.push(newTaskObject)
+        loopAndShowOnScreen()
+    };
+};
 
-    //when user presses delete icon
-    deleteIcon.addEventListener("click", () => {
-        containerDiv.classList.add("deleted");
-        spanTag.classList.add("deleted")
-        ulElement.addEventListener("animationend", () => {
-            newLiElement.remove();
+function loopAndShowOnScreen() {
+    //remove onload text
+    if (ulElement.contains(onloadText)) {
+        onloadText.remove();
+        document.querySelector("#ulElement").innerHTML = "";
+    };
+    removeIfExists()
+
+    //loop through the array and print out any new task
+    listOfToDos.forEach(todo => {
+        const newLiElement = document.createElement("li");
+        newLiElement.classList.add("list-item");
+        newLiElement.setAttribute("draggable", "true");
+        const spanTag = document.createElement("span");
+        spanTag.classList.add("fas");
+        spanTag.classList.add("fa-bars");
+        newLiElement.appendChild(spanTag);
+        //create the div container and the p and span tags inside it
+        const containerDiv = document.createElement("div");
+        containerDiv.classList.add("todo-item");
+        newLiElement.appendChild(containerDiv);
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.classList.add("checkbox");
+        containerDiv.appendChild(checkbox);
+        const pElement = document.createElement("p");
+        pElement.innerText = todo.inputedText;
+        containerDiv.appendChild(pElement);
+        const deleteIcon = document.createElement("label");
+        deleteIcon.setAttribute("for", "delete");
+        deleteIcon.classList.add("delete");
+        deleteIcon.innerHTML = '<i class="fas fa-trash"></i>';
+        const btnElement = document.createElement("input");
+        btnElement.type = "button";
+        btnElement.classList.add("btn");
+        btnElement.setAttribute("name", "delete");
+        deleteIcon.appendChild(btnElement);
+        containerDiv.appendChild(deleteIcon);
+        newLiElement.appendChild(containerDiv);
+        ulElement.appendChild(newLiElement);
+        userTextSubmission.value = "";
+
+        //when user presses delete icon
+        deleteIcon.addEventListener("click", () => {
+            listOfToDos.splice(todo, 1);
+            containerDiv.classList.add("deleted");
+            spanTag.classList.add("deleted")
+            ulElement.addEventListener("animationend", () => {
+                newLiElement.remove();
+                checkIfEmpty();
+            });
         });
-    });
 
-    //when user checks a spacific task
-    checkbox.addEventListener("click", () => {
-        if (checkbox.checked === true) {
-            pElement.classList.add("completed");
-            deleteIcon.classList.add("label-completed");
-        } else {
-            pElement.classList.remove("completed");
-            deleteIcon.classList.remove("label-completed");
-        };
-    });
-    //fire the event listeners right after creating a new li element
-    evenetListeners();
+        //when user marks task as "completed"
+        checkbox.addEventListener("click", () => {
+            if (checkbox.checked === true) {
+                pElement.classList.add("completed");
+                deleteIcon.classList.add("label-completed");
+            } else {
+                pElement.classList.remove("completed");
+                deleteIcon.classList.remove("label-completed");
+            };
+        });
+        //fire the event listeners right after creating a new li element to activate the drag&drop sotring functionality
+        evenetListeners();
+    })
+};
+
+//function to check if ul element is empty and create <p> tag if it is
+function checkIfEmpty() {
+    if (ulElement.innerHTML === "") {
+        nomoreTasks = document.createElement("p");
+        nomoreTasks.classList.add("onload-text");
+        nomoreTasks.id = "nomoreTasks";
+        nomoreTasks.innerHTML = "Woho! Looks like you're all done for today!";
+        ulElement.appendChild(nomoreTasks);
+    };
+};
+
+// remove the "you're done for today" text if it exists
+function removeIfExists() {
+    if (ulElement.hasChildNodes("#nomoreTasks")) {
+        ulElement.innerHTML = "";
+    };
 };
 
 
 //event listeners 
 
-inputForm.addEventListener("submit", () => { createNewTodo(); });
-submitButon.addEventListener("click", () => { createNewTodo(); });
+inputForm.addEventListener("submit", () => { createNewTodo(), false; });
+submitButon.addEventListener("click", () => { createNewTodo(), false; });
 
 submitButon.addEventListener("mouseover", () => {
     svgIcon.setAttribute("height", "1.4rem");
@@ -220,3 +243,23 @@ function evenetListeners(e) {
 }
 //fire the event listeners on page load
 evenetListeners();
+//print out pre-defined tasks
+// listOfToDos.forEach(task => createNewTodo);
+
+
+function numberIncruments() {
+    let startNumber = 4;
+    return startNumber + 1;
+};
+
+
+// function loopAndPrint() {
+//     for (let i = 0; i < listOfToDos.length; i++) {
+//         listOfToDos[i].createNewTodo;
+//         console.log(listOfToDos[i].inputedText);
+//     };
+// };
+// loopAndPrint();
+
+
+//pushing pre-defined tasks to array
